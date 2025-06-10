a4ac6001e69d17cb2cdd651dc25f1ffe54648bef
fd <dalirani.1373@gmail.com>
2025-06-10 01:24:43 -0400

    Added Custom Exception Handling and Logging.

---
+++ networksecurity/exception/exception.py
@@ -0,0 +1,15 @@
+import sys
+from networksecurity.logging import logger
+
+class NetworkSecurityException(Exception):
+    def __init__(self, error_message):
+        self.error_message = error_message
+        _, _, exc_tb = sys.exc_info()
+        self.lineno = exc_tb.tb_lineno
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
+
+    def __str__(self):
+        return "Error occurred in python script name [ {0} ], Line number [ {1} ], Error message [ {2} ]".format(
+            self.file_name, self.lineno, str(self.error_message)
+        )
+    
