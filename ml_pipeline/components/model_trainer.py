a39d084f13e820deb67be2354e689396ec2f85a4
fd <dalirani.1373@gmail.com>
2025-07-05 17:09:24 -0400

    Added local mlflow experiment tracking

--- ml_pipeline/components/model_trainer.py
+++ ml_pipeline/components/model_trainer.py
@@ -1,5 +1,7 @@
 import os
 
+import mlflow.sklearn
+
 from ml_pipeline.logging.logger import logging
 from ml_pipeline.exception.exception import MLPipelineException
 
@@ -15,7 +17,8 @@ from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier
-
+import mlflow
+from mlflow.models import infer_signature
 
 class ModelTrainer:
     def __init__(self, 
@@ -37,6 +40,88 @@ class ModelTrainer:
         except Exception as e:
             raise MLPipelineException(e)
     
+    def track_mlflow(self,
+                     best_model,
+                     train_metrics: ModelMetricArtifact = None,
+                     test_metrics: ModelMetricArtifact = None,
+                     input_example=None):
+        """
+        Logs training and testing metrics, along with the model, to MLflow.
+
+        Parameters:
+        ----------
+        best_model : Any
+            The trained model to be logged.
+
+        train_metrics : ModelMetricArtifact, optional
+            Evaluation metrics for the training dataset.
+
+        test_metrics : ModelMetricArtifact, optional
+            Evaluation metrics for the testing dataset.
+
+        input_example : pd.DataFrame or np.ndarray, optional
+        Example input for the model used to infer the signature and for logging.
+
+        Raises:
+        ------
+        ValueError
+            If neither training nor testing metrics are provided.
+        NotImplementedError
+            If regression metrics logging is requested but not implemented.
+        """
+
+        if not train_metrics and not test_metrics:
+            raise ValueError("At least one of train_metrics or test_metrics must be provided.")
+
+        with mlflow.start_run():
+            if train_metrics:
+                self._log_metrics_to_mlflow(train_metrics, prefix="train")
+
+            if test_metrics:
+                self._log_metrics_to_mlflow(test_metrics, prefix="test")
+
+            signiture = None
+            if input_example is not None:
+                try:
+                    output_example = best_model.predict(input_example)
+                    signiture = infer_signature(model_input=input_example, model_output=output_example)
+                except Exception as e:
+                    logging.info("Failed to infer model signiture.")
+
+            mlflow.sklearn.log_model(
+                sk_model=best_model, 
+                artifact_path="model",
+                signature=signiture,
+                input_example=input_example
+            )
+
+    def _log_metrics_to_mlflow(self, metric_artifact: ModelMetricArtifact, prefix: str):
+        """
+        Logs classification metrics to MLflow with a given prefix.
+
+        Parameters:
+        ----------
+        metric_artifact : ModelMetricArtifact
+            The metrics to log.
+        prefix : str
+            Prefix to distinguish metrics (e.g., 'train' or 'test').
+        """
+
+        if metric_artifact.classification_metrics:
+            cm = metric_artifact.classification_metrics
+            metrics = {
+                f"{prefix}_f1_score": cm.f1_score,
+                f"{prefix}_precision_score": cm.precision_score,
+                f"{prefix}_recall_score": cm.recall_score,
+            }
+            mlflow.log_metrics(metrics)
+
+        elif metric_artifact.regression_metrics:
+            raise NotImplementedError("Regression metrics logging is not implemented.")
+        else:
+            raise ValueError("Provided metric artifact contains no valid metrics.")
+    
+
     def train_model(self, X_train, y_train, X_test, y_test):
         """
         Trains a set of machine learning models, evaluates them, selects the best model, 
@@ -118,8 +203,16 @@ class ModelTrainer:
         else:
             raise ValueError('Selected ML Model Type [{}] is not correct!'.format(self.model_trainer_config.model_type))
 
-        ## Track expriment with MLFLOW
+        # Input example: does not anonymize data
+        input_examlpe = X_train[0: 5]
 
+        # Track expriment with MLFLOW
+        self.track_mlflow(
+            best_model=best_model,
+            train_metrics = trainset_model_metric_artifact,
+            test_metrics = testset_model_metric_artifact,
+            input_example = input_examlpe
+        )
 
         # Load the data preprocessor used during feature transformation
         preprocessor = load_object(file_path=self.data_transform_artifact.transformed_object_file_path)
